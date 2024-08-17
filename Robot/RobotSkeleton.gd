2484dfcaae52c84855dbb86190aece33c64b4802
Tarin <77469919+T4R1N@users.noreply.github.com>
2024-08-16 22:24:17 -0500

    Groundwork for skeleton
    yuh

---
+++ Robot/RobotSkeleton.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name RobotSkeleton
+
+@export var arms: Array[RobotArm]
+@export var legs: Array[RobotLeg]
+@export var augments: Array[RobotAugment]
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var skeleton: RobotSkeleton
+
 const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
