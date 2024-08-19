3492375199acad92bc7246e7654653d28ddfd0c1
Tarin <77469919+T4R1N@users.noreply.github.com>
2024-08-18 21:40:50 -0500

    Item pickups

--- Inventory/InventoryDatabase.gd
+++ Inventory/InventoryDatabase.gd
@@ -1,3 +1,10 @@
 extends Node
 
 @export var inv: Array[AbstractRobotPiece]
+
+func pickup(piece: AbstractRobotPiece):
+	for slot in inv:
+		if slot == null:
+			slot = piece
+			print(slot)
+			break
--- Inventory/Item.gd
+++ Inventory/Item.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+@onready var inventory = get_node("/root/Main/InventoryDatabase")
 @export var item: AbstractRobotPiece # Will be set when randomly generated
 
 var gravity = 1600
+
+func pickup():
+	inventory.pickup(item)
+	queue_free()
 
 func _ready():
 	$Sprite2D.texture = item.texture
@@ -10,3 +15,9 @@ func _ready():
 func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += gravity * delta
+		
+	for body in $PickupZone.get_overlapping_bodies():
+		if body is Player:
+			pickup()
+			
+	move_and_slide()
